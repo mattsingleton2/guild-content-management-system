ead0f6928a1491085a02cb880b8025a13418db30
Matt Singleton <matt.singleton2@gmail.com>
2021-09-16 11:03:59 -0500

    fixing social icons across all pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,12 +10,5 @@ $(function() {
     });
 
     $(".carousel").carousel( { interval: 2000 } );
-    //Abdulai, I've commented your script because it doesn't work and breaks the functionality on the other lines. Fix it first and then we're good to go.
-    // function selection(){
-    //     var selected=document.getElementById("#inputGroupSelect").value;
-    //     if(selected==0){
-    //       document.getElementById("#input1").removeAttribute("hidden");
-    //     }else{
-    //       //elsewhere actions
-    //     };
+
 });
